21afb49db1d742b52a8c3e008a7a63ebffb589f7
Nidhi Elango <nidhielango3@gmail.com>
2021-12-28 20:33:57 -0500

    Update layout

--- src/components/RouteList.vue
+++ src/components/RouteList.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Route List</h1>
+    <section class="route-list">
+        <h1>Route List</h1>
+    </section>    
 </template>
+
+<style scoped>
+.route-list {
+    position:relative;
+    z-index:1;
+    max-width:250px;
+    margin:10px;
+    background-color:white;
+}
+</style>
